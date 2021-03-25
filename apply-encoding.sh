8a3c9342dece64ce2140b06288d6ffdd00b19c72
German Fica <germafica@gmail.com>
2021-03-25 17:24:39 -0300

    New update apply-encoding.sh

--- apply-encoding.sh
+++ apply-encoding.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#enter input encoding here
-FROM_ENCODING="value_here"
-#output encoding(UTF-8)
-TO_ENCODING="UTF-8"
 #message 1
 msg1='Old files have been deleted successfully.'
 #message 2
@@ -17,7 +13,7 @@ done
 echo $msg1
 #loop to change name of multiple new files
 for  file  in  *.utf8.converted; do
-	rm "$file"
+	mv "$file" "${file%.htm}.htm"
 done
 #message 2
 echo $msg2
