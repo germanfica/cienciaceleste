6d5a6343db82f3d1caf1366a2734702baf894653
German Fica <germafica@gmail.com>
2021-03-25 18:12:42 -0300

    New encoding.sh update

--- encoding.sh
+++ encoding.sh
@@ -7,6 +7,10 @@ TO_ENCODING="UTF-8"
 CONVERT=" iconv  -f   $FROM_ENCODING  -t   $TO_ENCODING"
 #loop to convert multiple files 
 for  file  in  *.htm; do
-	     $CONVERT   "$file"   -o  "${file%.htm}.utf8.converted"
-     done
-     exit 0
+	$CONVERT   "$file"   -o  "${file%.htm}.utf8.converted"
+done
+echo Applying encoding
+#external script
+sh ./apply-encoding.sh
+#exit
+exit 0
