327f4b925524c6cd7e5a689ff37ea2e3afeb046f
German Fica <germafica@gmail.com>
2021-03-25 17:06:06 -0300

    New messages
    Ahora el script muestra unos mensajes para saber si loa operación de
    borrado y renombre fue un éxito

--- apply-encoding.sh
+++ apply-encoding.sh
@@ -3,13 +3,19 @@
 FROM_ENCODING="value_here"
 #output encoding(UTF-8)
 TO_ENCODING="UTF-8"
+#message 1
+msg1='Old files have been deleted successfully.'
+#message 2
+msg2='New files have been renamed successfully.'
 #loop to remove multiples old files
 for  file  in  *.htm; do
 	     rm "$file"
      done
      exit 0
+echo "$msg1"
 #loop to change name of multiple new files
 for  file  in  *.utf8.converted; do
 	     rm "$file"
      done
      exit 0
+echo "$msg2"
