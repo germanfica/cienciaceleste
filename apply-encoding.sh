54c37463e615fefa410bd72f2775c85a421f205b
German Fica <germafica@gmail.com>
2021-03-25 16:42:33 -0300

    Working on apply encoding feature
    Estoy haciendo el script para eliminar los viejos archivos y renombrar
    los nuevos archivos con el nombre de los anteriores.

---
+++ apply-encoding.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#enter input encoding here
+FROM_ENCODING="value_here"
+#output encoding(UTF-8)
+TO_ENCODING="UTF-8"
+#convert
+
+#loop to change name of multiple files 
+#loop to remove multiples old files
+for  file  in  *.htm; do
+	     rm "$file"
+     done
+     exit 0
