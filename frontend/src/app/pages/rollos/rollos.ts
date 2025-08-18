c431f1ebc475fc15692d805fd93c980b486c2eb4
German Fica <germafica@gmail.com>
2025-08-18 01:04:32 -0300

    Add RouterModule in rollos component

--- frontend/src/app/pages/rollos/rollos.ts
+++ frontend/src/app/pages/rollos/rollos.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-rollos',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './rollos.html',
   styleUrl: './rollos.scss'
 })
