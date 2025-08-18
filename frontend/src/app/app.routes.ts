bcede6cc474b3ef60ad8bb98dee7170dd68e86b0
German Fica <germafica@gmail.com>
2025-08-18 00:54:31 -0300

    Add rollo-detalle component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from "./pages/home/home";
 import { Rollos } from "./pages/rollos/rollos";
+import { RolloDetalle } from './pages/rollo-detalle/rollo-detalle';
 
 export const routes: Routes = [
   { path: "", component: Home, title: "Divinos Rollos Telepáticos - Inicio" },
   { path: "rollos", component: Rollos, title: "Divinos Rollos Telepáticos - Listado" },
+  { path: "rollos/:id", component: RolloDetalle, title: "Divinos Rollos Telepáticos - Detalle" },
   { path: "**", redirectTo: "" }
 ];
---
+++ frontend/src/app/pages/rollo-detalle/rollo-detalle.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rollo-detalle',
+  imports: [],
+  templateUrl: './rollo-detalle.html',
+  styleUrl: './rollo-detalle.scss'
+})
+export class RolloDetalle {
+  titulo = "MENSAJE TELEPÁTICO DEL PADRE ETERNO AL MUNDO TERRESTRE; MENSAJE SEGUNDO; EL PRIMER MENSAJE FUÉ OCULTADO AL MUNDO POR LA ROCA RELIGIOSA.-";
+}
