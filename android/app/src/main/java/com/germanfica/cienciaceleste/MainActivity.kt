ac517281742bcded2005d7b82f661cf5fc77c588
German Fica <germafica@gmail.com>
2026-01-03 22:01:41 -0300

    Add Android app
    Add a complete Android application scaffold using Gradle Kotlin DSL and
    Jetpack Compose.
    
    Notes:
    - Target SDK set to 36 with minimum SDK 21
    - Uses version catalogs for dependency and plugin management
    - Initial UI displays a simple greeting placeholder

---
+++ android/app/src/main/java/com/germanfica/cienciaceleste/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.germanfica.cienciaceleste
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.germanfica.cienciaceleste.ui.theme.CienciaCelesteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CienciaCelesteTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CienciaCelesteTheme {
+        Greeting("Android")
+    }
+}
