36c18fccbece380ea39f975e3f901992123ea80f
German Fica <germafica@gmail.com>
2026-09-02 00:33:32 -0300

    Add admin-rollos

---
+++ frontend/src/app/admin/admin-rollos/admin-rollos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-rollos',
+  imports: [],
+  templateUrl: './admin-rollos.html',
+  styleUrl: './admin-rollos.scss',
+})
+export class AdminRollos {}
