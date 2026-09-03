1a63b810c720baddd094d3b07d8a3447b8bc73e2
German Fica <germafica@gmail.com>
2026-09-02 22:48:35 -0300

    Add admin-divinos-minirollos component

---
+++ frontend/src/app/admin/admin-divinos-minirollos/admin-divinos-minirollos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-divinos-minirollos',
+  imports: [],
+  templateUrl: './admin-divinos-minirollos.html',
+  styleUrl: './admin-divinos-minirollos.scss',
+})
+export class AdminDivinosMinirollos {}
