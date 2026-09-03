4ca2edd10ccd5fbf213fce73952c0e7b3f83b796
German Fica <germafica@gmail.com>
2026-09-03 01:19:59 -0300

    Add divine laws administration component

---
+++ frontend/src/app/admin/admin-divinas-leyes/admin-divinas-leyes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-divinas-leyes',
+  imports: [],
+  templateUrl: './admin-divinas-leyes.html',
+  styleUrl: './admin-divinas-leyes.scss',
+})
+export class AdminDivinasLeyes {}
