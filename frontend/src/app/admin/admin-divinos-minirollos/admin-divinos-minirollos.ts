22d7e7dbd003b3edff16872541fc10371507a23b
German Fica <germafica@gmail.com>
2026-09-02 23:26:23 -0300

    Delte admin-divinos-minirollos component

--- frontend/src/app/admin/admin-divinos-minirollos/admin-divinos-minirollos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-admin-divinos-minirollos',
-  imports: [],
-  templateUrl: './admin-divinos-minirollos.html',
-  styleUrl: './admin-divinos-minirollos.scss',
-})
-export class AdminDivinosMinirollos {}
