2c8ff9e5e4681b17fc13b539c0378b2a6f77fa89
German Fica <germafica@gmail.com>
2026-09-04 00:19:37 -0300

    Add admin-topbar component

---
+++ frontend/src/app/admin/admin-topbar/admin-topbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-topbar',
+  imports: [],
+  templateUrl: './admin-topbar.html',
+  styleUrl: './admin-topbar.scss',
+})
+export class AdminTopbar {}
